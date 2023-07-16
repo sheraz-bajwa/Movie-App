3a6f727737737811321e1d48a61cb16ca05d61e7
sheraz-bajwa <sheezybajwa@gmail.com>
2023-07-16 21:52:48 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movieapp/home.dart';
+import 'package:movieapp/home1.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
 void main() {
@@ -7,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: Home(),
     );
   }
 }
--- lib/trendingMovies.dart
+++ lib/trendingMovies.dart
@@ -10,40 +10,48 @@ class TrendingMovies extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('trending Moviesss'),
-            Container(
-              height: 300,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: trending.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      child: Container(
-                        height: 150,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Container(
-                            //   child: Text(
-                            //     trending[index]['title'],
-                            //     style: TextStyle(color: Colors.amber),
-                            //   ),
-                            // )
-                            Container(
-                              height: 200,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: NetworkImage(
-                                          'https://image.tmbd.org/t/p/w500' +
-                                              trending[index]['poster_path']))),
-                            )
-                          ],
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                height: 300,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: trending.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: 150,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 70,
+                                child: Text(
+                                  trending[index]['title'] != null
+                                      ? trending[index]['title']
+                                      : ['loading'],
+                                  style: TextStyle(
+                                      color: Colors.amber, fontSize: 20),
+                                ),
+                              )
+                              // Container(
+                              //   height: 200,
+                              //   decoration: BoxDecoration(
+                              //       image: DecorationImage(
+                              //           image: NetworkImage(
+                              //               'https://image.tmbd.org/t/p/w500' +
+                              //                   trending[index]['poster_path']))),
+                              // )
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
+              ),
             )
           ],
         ),
